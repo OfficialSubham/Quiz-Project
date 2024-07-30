d89422ea5e5715fff85ace0d0b30507b87031351
Subham <subhamoffi@gmail.com>
2024-07-30 08:00:10 +0530

    Much Work is done

--- JS File/dom.js
+++ JS File/dom.js
@@ -1,9 +1,10 @@
-import { answerChecker } from "./functions.js";
+import { answerChecker, correctOrNot } from "./functions.js";
 
 document.querySelectorAll(".option").forEach((button) => {
         const answer = button.dataset.answer;
         button.addEventListener('click', () => {
             answerChecker(answer);
+            correctOrNot();
             console.log(answer);
         })
-    })
+})
--- JS File/functions.js
+++ JS File/functions.js
@@ -9,8 +9,50 @@ export function answerChecker(choosenOption) {
     })
     if(!correctAns) {
         console.log("Wrong Answer");
+        noRedo()
+        
     }
     else{
         console.log("Correct Answer");
+        noRedo()
+
+       
     }
+
+    return correctAns;
+}
+
+function noRedo() {
+    document.querySelectorAll(".pointer")
+        .forEach((button) => {
+            button.classList.add("block")
+
+            button.classList.remove("pointer")
+        })
+}
+
+export function correctOrNot() {
+    document.querySelectorAll(".option")
+        .forEach((Answer) => {
+            let answer = Answer.dataset.answer;
+            let correct = answerChecker(answer)
+            if(correct == 1) {
+                document.querySelectorAll(".option")
+                    .forEach((button) => {
+                        let dataAnswer = button.dataset.answer;
+                        if(dataAnswer == answer) {
+                            button.classList.add("correct-answer")
+                        }
+                    })
+            }
+            else{
+                document.querySelectorAll(".option")
+                .forEach((button) => {
+                    let dataAnswer = button.dataset.answer;
+                    if(dataAnswer == answer) {
+                        button.classList.add("wrong-answer")
+                    }
+                })
+            }
+        })
 }
